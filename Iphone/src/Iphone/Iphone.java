ce43c01d882bb539c1deac433723aa834d1a9e31
Rafael.Gogge <rafa.gogge@gmail.com>
2024-09-14 01:24:17 -0300

    Update Iphone.java

--- Iphone/src/Iphone/Iphone.java
+++ Iphone/src/Iphone/Iphone.java
@@ -1,16 +1,16 @@
 package Iphone;
 
-//import Iphone.funcionalidades.AparelhoTelefonico;
+import Iphone.funcionalidades.AparelhoTelefonico;
 import Iphone.funcionalidades.ReprodutorMusical;
 
 public class Iphone {
     public static void main(String[] args) throws Exception {
 
-        //AparelhoTelefonico meuAparelho = new AparelhoTelefonico();
+        AparelhoTelefonico meuAparelho = new AparelhoTelefonico();
         ReprodutorMusical meuReprodutorMusical = new ReprodutorMusical();
 
-        //meuAparelho.iniciarChamada();
-       //meuAparelho.atenderChamada();
+        meuAparelho.iniciarChamada();
+        meuAparelho.atenderChamada();
         meuReprodutorMusical.selecionarMusica();
     }
 }
