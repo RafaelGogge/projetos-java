eba77c4f8065ee30fd9e15ff8b4f3b92a1b637f2
Rafael.Gogge <rafa.gogge@gmail.com>
2024-09-14 21:58:23 -0300

    Navegador web adicionado

--- Iphone/src/Iphone/Iphone.java
+++ Iphone/src/Iphone/Iphone.java
@@ -1,16 +1,19 @@
 package Iphone;
 
-import Iphone.funcionalidades.AparelhoTelefonico;
-import Iphone.funcionalidades.ReprodutorMusical;
+//import Iphone.funcionalidades.AparelhoTelefonico;
+import Iphone.funcionalidades.NavegadorInternet;
+//import Iphone.funcionalidades.ReprodutorMusical;
 
 public class Iphone {
     public static void main(String[] args) throws Exception {
 
-        AparelhoTelefonico meuAparelho = new AparelhoTelefonico();
-        ReprodutorMusical meuReprodutorMusical = new ReprodutorMusical();
+        //AparelhoTelefonico meuAparelho = new AparelhoTelefonico();
+        //ReprodutorMusical meuReprodutorMusical = new ReprodutorMusical();
+        NavegadorInternet meuNavegador = new NavegadorInternet();
 
-        meuAparelho.iniciarChamada();
-        meuAparelho.atenderChamada();
-        meuReprodutorMusical.selecionarMusica();
+        //meuAparelho.iniciarChamada();
+        //meuAparelho.atenderChamada();
+        //meuReprodutorMusical.selecionarMusica();
+        meuNavegador.NavegadorWeb();
     }
 }
