01ad471dd2c0ec3e7ad7e77df9497b72f300f71c
Rafael.Gogge <rafa.gogge@gmail.com>
2024-09-18 23:03:08 -0300

    Update ContaBanco.java

--- conta-bancaria/src/ContaBanco.java
+++ conta-bancaria/src/ContaBanco.java
@@ -19,6 +19,7 @@ public class ContaBanco {
             System.out.println(String.format(
                 "Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d, e seu saldo %.2f já está disponível para saque.",
                 nomeCliente, agencia, numero, saldo
+            
             ));
         }
     }
